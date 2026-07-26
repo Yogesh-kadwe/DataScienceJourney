ae30230acb1de1e64d465d66c16d3290e05e3ec1
Yogesh-kadwe <kadweyogesh.cse24@sbjit.edu.in>
2026-07-26 18:48:29 +0530

    Updated code

---
+++ Python/06_Condtional.py
@@ -0,0 +1,111 @@
+
+
+
+# ---------------------------------------------#
+
+
+
+
+# # 1 to 10
+# num=int(input("Enter a number : "))
+# i=0
+# while i<=num-1:
+#     i+=1
+#     print(i)
+    
+
+
+
+# 10 to 1
+
+# num=int(input("Enter a number : "))
+# i=0
+# while i<num:
+#     print(num-i)
+#     i+=1
+    
+# i = 10
+
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# EVEN NUMBER
+
+# num=int(input("Enter a number : "))
+# i = 2
+# while i <= num:
+#     print(i)
+#     i += 2
+
+
+
+# sum of number
+# num=int(input("Enter a number : "))
+# sum=0
+# i = 0
+# while i <= num-1:
+#     i+=1
+#     sum += i
+# print("the total sum of this number is : ",sum)
+
+# ---------------------------------------------#
+
+
+# Write a program to calculate the factorial of a number.
+# num=int(input("Enter a number : "))
+# fact=1
+# i = 0
+# while i <num:
+#     i+=1
+#     fact *= i
+
+# print("the factorial of this number is : ",fact)
+
+
+
+# second method
+# i = 1
+# fact = 1
+
+# while i <= num:
+#     fact *= i
+#     i += 1
+
+
+# ---------------------------------------------#
+
+# Write a program to print the multiplication table of any number
+# num=int(input("Enter a table number that want to print : "))
+# i=1
+# while i<=10:
+#     print(f"{num} * {i} = {num * i}")
+#     i+=1
+
+
+# ---------------------------------------------#
+# Write a program to count how many digits are present in a number.
+
+# num=input("Enter a number : ")
+# print(len(num))
+
+num=int(input("Enter a number : "))
+i=0
+while i<=num:
+    print(i)
+    
+# ---------------------------------------------#
+
+
+
+
+
+# ---------------------------------------------#
+
+
+
+
+
+
+
+# ---------------------------------------------#
