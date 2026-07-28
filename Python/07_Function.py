c554b2aa97ed014ae41551679bd07f698f3ea85b
Yogesh-kadwe <kadweyogesh.cse24@sbjit.edu.in>
2026-07-28 20:50:08 +0530

    Updated code

--- Python/07_Function.py
+++ Python/07_Function.py
@@ -63,11 +63,127 @@
 
 
 
-def largest(a,b):
-    if a>b:
-        return a
-    else:
-        return b
+# def largest(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
 
-result=largest(10,25)
+# result=largest(10,25)
+# print(result)
+
+
+
+ # ---------------------------------------------#
+
+            # LAMBDA FUNCTION
+
+ # ---------------------------------------------#
+
+# add = lambda a,b: a+b
+# print(add(15,25))
+
+
+# square =lambda x:x*x
+# print(square(9))
+
+# largest = lambda a,b:a if a>b else b
+# print(largest(40,25))
+
+
+ # ---------------------------------------------#
+       # MAP FUNCTION
+
+# Program 1
+# Double every number in the list.
+
+# numbers= [2,4,6,8]
+# result=list(map(lambda x:10+x,numbers))
+# print(result)
+
+
+
+
+
+# Program 2
+# Find the square of every number
+
+# numbers=[1,2,3,4,5,6]
+# result=list(map(lambda x:x*x,numbers))
+# print(result)
+
+
+
+
+
+
+# Program 3
+# Convert every name to uppercase.
+
+# names = ["yogesh", "rahul", "amit"]
+# result=list(map(lambda x:len(x),names))
+# print(result)
+  # ------------------
+  # FILTER FUNCTION 
+
+# Keep only odd numbers.
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+
+# result = list(filter(lambda x: x % 2 != 0, numbers))
+
+# print(result)
+
+
+
+# # Keep numbers greater than 50.
+# numbers = [10, 45, 60, 70, 20, 90]
+# result=list(filter(lambda x:x>50,numbers))
+# print(result)
+
+# # Keep names with more than 5 characters.
+# names = ["Ram", "Yogesh", "Rahul", "Amit", "Prakash"]
+# result=list(filter(lambda x:len(x)>5,names))
+# print(result)
+
+
+  # ----------------------------------------------#
+
+        # REDUCE() FUNCTION
+
+# # Find the sum of all numbers.
+# from functools import reduce
+# numbers = [10, 20, 30, 40]
+# result=reduce(lambda a,b :a+b ,numbers)
+# print(result)
+
+
+# # Find the product of all numbers.
+# from functools import reduce
+# numbers = [2, 3, 5]
+# result=reduce(lambda a,b:a*b,numbers)
+# print(result)
+
+
+# # Find the smallest number.
+# from functools import reduce
+# numbers = [45, 10, 80, 25]
+# result=reduce(lambda a,b: a if a<b else b,numbers)
+# print(result)
+
+from functools import reduce
+numbers = [25, 80, 15, 90, 60]
+result=reduce(lambda a,b:a if a>b else b,numbers)
 print(result)
+
+   # ---------------------------------------------#
+
+
+
+
+    # ---------------------------------------------#
+
+
+
+
+
+     # ---------------------------------------------#
