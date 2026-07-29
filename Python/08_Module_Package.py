503cd47be788607430891ae75fcd0386e90239c2
Yogesh-kadwe <kadweyogesh.cse24@sbjit.edu.in>
2026-07-29 09:39:20 +0530

    Updated Code

---
+++ Python/08_Module_Package.py
@@ -0,0 +1,18 @@
+# Program 1
+# Import the math module and print:
+# Square root of 81
+# Factorial of 6
+import math
+print(math.sqrt(81))
+print(math.factorial(6))
+
+# Program 2
+# Import the random module and print a random number between 1 and 100.
+# (Hint: Use randint().)
+import random
+print(random.randint(1,100))
+
+# Program 3
+# Import the math module using an alias m and print the value of π.
+import math as m
+print(m.pi)
