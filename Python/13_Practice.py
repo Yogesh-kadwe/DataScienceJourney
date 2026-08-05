6444713535d42913ad5d93a5f4e5e6757b1e10ca
Yogesh-kadwe <kadweyogesh.cse24@sbjit.edu.in>
2026-08-05 22:05:55 +0530

    Updated Code

--- Python/13_Practice.py
+++ Python/13_Practice.py
@@ -45,13 +45,61 @@
 
 
 
-# Write a program to calculate the factorial of a number.
-num=int(input("Enter a number : "))
-fact=1
-i = 0
-while i <num:
-    i+=1
-    fact *= i
+# # Write a program to calculate the factorial of a number.
+# num=int(input("Enter a number : "))
+# fact=1
+# i = 0
+# while i <num:
+#     i+=1
+#     fact *= i
 
-print("the factorial of this number is : ",fact)
+# print("the factorial of this number is : ",fact)
 
+
+
+# # Check if a number is a palindrome
+# num=input("Enter a Number: ")
+# reverse=num[::-1]
+# if num==reverse:
+#     print("Number is Pallindrome")
+# else:
+#     print("Number is not Pallindrome")
+
+
+
+
+# #  Write a program to find the sum of first N natural numbers.
+# n=int(input("Enter a Number : "))
+# sum=0
+# for i in range(0,n+1):
+#     sum+=i
+# print("Sum :",sum)
+
+
+
+
+
+
+
+# #  Write a program to print all prime numbers between 1 and 100. 
+# for num in range(2, 101):
+#     prime = True
+
+#     for i in range(2, num):
+#         if num % i == 0:
+#             prime = False
+#             break
+
+#     if prime:
+#         print(num, end=" ")
+
+
+
+
+# Easy method prime number
+for num in range(2, 101):
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=" ")
