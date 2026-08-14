5ae00ea34d61c9971faf96087c7a92979d94759f
Yogesh-kadwe <kadweyogesh.cse24@sbjit.edu.in>
2026-08-14 06:10:49 +0530

    Updated code

--- Python/14_SQL.py
+++ Python/14_SQL.py
@@ -29,3 +29,79 @@
 
 
 
+# # Schema:
+
+# # Customer (cname, street, city)
+# # Deposit (cname, accno)
+# # Loan (loanno, bname, amt)
+# # Borrow (cname, loanno)
+# # Find all customer names having either an account or a loan or both.
+
+# SELECT cname
+# FROM Deposit
+
+# UNION
+
+# SELECT cname
+# FROM Borrow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Customer (cname, street, city)
+# Deposit (cname, accno)
+# Loan (loanno, bname, amt)
+# Borrow (cname, loanno)
+# Find names of customers having either an account or a loan or both.
+
+
+
+
+
+
+
+
+
+
+
+
+# # Find all city names containing the character n.
+# SELECT city
+# FROM Customer
+# WHERE city LIKE '%n%';
+
+
+
+
+# # Find all branch names whose names are not ending with a.
+
+# SELECT bname
+# FROM Loan
+# WHERE bname NOT LIKE '%a';
+
+
+
+
+
+
+
+
+# # What are string functions?
+
+# String functions are SQL functions used to perform operations 
+# on character or text data, such as changing case, finding length, 
+# joining strings, and extracting portions of strings
